1ef86a79f6906a1e91485b3ea63b5b93fdaea0eb
aangairbender <jetixranger@ya.ru>
2016-03-31 08:05:54 +0300

    weqwe

---
+++ Destiny/InputController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Destiny
+{
+    class InputController
+    {
+        private World world;
+        public bool mouseDowned;
+        public Point mouseLocation;
+        public InputController(World world)
+        {
+            this.world = world;
+        }
+        public void keyDown(KeyEventArgs e)
+        {
+            if (world.heroController.makeMove(e.KeyCode)) world.makeMoves();
+        }
+        public void keyUp(KeyEventArgs e)
+        {
+
+        }
+        public void mouseDown(MouseEventArgs e)
+        {
+            mouseDowned = true;
+            mouseLocation = e.Location;
+        }
+        public void mouseUp(MouseEventArgs e)
+        {
+            mouseDowned = false;
+            mouseLocation = e.Location;
+        }
+        public void mouseMove(MouseEventArgs e)
+        {
+            mouseLocation = e.Location;
+        }
+    }
+}
